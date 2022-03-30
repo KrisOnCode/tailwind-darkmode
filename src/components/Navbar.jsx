5dfea75c4fe0c858f2cd4083304653d309897ebb
KrisOnCode <krispaschal@gmail.com>
2022-03-29 20:32:12 -0500

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import useDarkMode from '../hooks/useDarkmode';
+import ThemeToggler from './ThemeToggler';
+
+export default function Navbar() {
+    const [darkMode, setDarkMode] = useDarkMode();
+    return (
+        <>
+          <nav className="font-sans flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 px-6 bg-white dark:bg-black shadow sm:items-baseline w-full">
+                <div className="mb-2 sm:mb-0">
+                    <a href="/home" className="text-2xl no-underline text-slate-900 dark:text-white">Tailwind Darkmode</a>
+                </div>
+                <div>
+                    <ThemeToggler darkMode={darkMode} setDarkMode={setDarkMode} />
+                </div>
+            </nav> 
+        </>
+    )
+}
+
+       
